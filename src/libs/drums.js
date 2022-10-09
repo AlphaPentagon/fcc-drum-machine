0173e531457be22454b79ceda04721eb71c1d4d5
Craig Summers <73234010+AlphaPentagon@users.noreply.github.com>
2022-10-09 21:02:05 +0100

    Updated file names

--- src/libs/drums.js
+++ src/libs/drums.js
@@ -2,47 +2,47 @@ const drums = [
   {
     letter: "Q",
     name: "Heater 1",
-    sound_path: "/audio/Heater-1.mp3",
+    sound_path: "%PUBLIC_URL%/audio/Heater-1.mp3",
   },
   {
     letter: "W",
     name: "Heater 2",
-    sound_path: "/audio/Heater-2.mp3",
+    sound_path: "%PUBLIC_URL%/audio/Heater-2.mp3",
   },
   {
     letter: "E",
     name: "Heater 3",
-    sound_path: "/audio/Heater-3.mp3",
+    sound_path: "%PUBLIC_URL%/audio/Heater-3.mp3",
   },
   {
     letter: "A",
     name: "Heater 4",
-    sound_path: "/audio/Heater-4_1.mp3",
+    sound_path: "%PUBLIC_URL%/audio/Heater-4_1.mp3",
   },
   {
     letter: "S",
     name: "Hi-Hat",
-    sound_path: "/audio/Cev_H2.mp3",
+    sound_path: "%PUBLIC_URL%/audio/Cev_H2.mp3",
   },
   {
     letter: "D",
     name: "Open Hi-Hat",
-    sound_path: "/audio/Dsc_Oh.mp3",
+    sound_path: "%PUBLIC_URL%/audio/Dsc_Oh.mp3",
   },
   {
     letter: "Z",
     name: "Clap",
-    sound_path: "/audio/Heater-6.mp3",
+    sound_path: "%PUBLIC_URL%/audio/Heater-6.mp3",
   },
   {
     letter: "X",
     name: "Kick",
-    sound_path: "/audio/RP4_KICK_1.mp3",
+    sound_path: "%PUBLIC_URL%/audio/RP4_KICK_1.mp3",
   },
   {
     letter: "C",
     name: "Kick & Hat",
-    sound_path: "/audio/Kick_n_Hat.mp3",
+    sound_path: "%PUBLIC_URL%/audio/Kick_n_Hat.mp3",
   },
 ];
 
